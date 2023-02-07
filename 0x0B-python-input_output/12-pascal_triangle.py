ca18421d494bd0ddcbb11eb1d90c95a8fdf6d0f2
giantcobat <prinxtachie@gmail.com>
2023-02-07 12:18:15 +0000

    com

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
+""" Creating pascal triangle function """
 
 
 def pascal_triangle(n):
@@ -17,4 +17,4 @@ def pascal_triangle(n):
             tmp.append(tri[i] + tri[i + 1])
         tmp.append(1)
         triangles.append(tmp)
-    return 
+    return triangles
